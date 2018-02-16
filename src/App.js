a0b64972cab03bade8077cc9587b61d6b363b7b1
Rajesh <drajesh2089@gmail.com>
2018-02-15 16:26:09 -0800

    added existing fiels.

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+ //state is property which is reserved as word by react but we can put anything but keeping state is good.
+  state = {
+    persons:[
+      { name: "Rajesh", age: 28 },
+      { name: "Mahesh", age: 26 },
+      { name: "Rajitha", age: 31 }
+    ],
+    otherState:"somevaleu",
+      showPersons: false
+  }
+
+  switchNameHandler = (newName) => {
+   //DON'T DO THIS: react will not recognize  this.state.persons[0].name = "Dumpeti Rajesh";
+    this.setState ( {
+      persons: [
+      { name: newName, age: 28 },
+      { name: "Mahesh", age: 26 },
+      { name: "Rajitha", age: 32 }
+    ]
+  } )
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState ({
+      persons: [
+        { name: 'Rajesh', age: 28 },
+        { name: event.target.value, age: 26 },
+        { name: "Rajitha", age: 32 }
+      ]
+    })
+  }
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    } ;
+    let persons = null;
+
+    if (this.state.showPersons) {
+        persons = (
+            <div>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, "Brother of mahesh ")}
+                    changed={this.nameChangeHandler}> my hobbies: Racing </Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
+            </div>
+        )
+    }
+
+    return (
+      <div className="App">
+        <h1>First react app</h1>
+        <p>This is really working</p>
+        <button 
+          style= {style}
+          onClick={this.togglePersonHandler}>Toggle Persons</button>
+          {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
